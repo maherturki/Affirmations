453ff974e34517a6fea08fcb8bb838c6be2a69cd
Maher <maherturki25@gmail.com>
2021-11-02 16:12:55 +0100

    commit

---
+++ app/src/main/java/com/maher/affirmations/data/Datasource.kt
@@ -0,0 +1,21 @@
+package com.maher.affirmations.data
+
+import com.maher.affirmations.R
+import com.maher.affirmations.model.Affirmation
+
+class Datasource {
+    fun loadAffirmations(): List<Affirmation> {
+        return listOf<Affirmation>(
+            Affirmation(R.string.affirmation1),
+            Affirmation(R.string.affirmation2),
+            Affirmation(R.string.affirmation3),
+            Affirmation(R.string.affirmation4),
+            Affirmation(R.string.affirmation5),
+            Affirmation(R.string.affirmation6),
+            Affirmation(R.string.affirmation7),
+            Affirmation(R.string.affirmation8),
+            Affirmation(R.string.affirmation9),
+            Affirmation(R.string.affirmation10)
+        )
+    }
+}
---
+++ app/src/main/java/com/maher/affirmations/model/Affirmation.kt
@@ -0,0 +1,4 @@
+package com.maher.affirmations.model
+
+data class Affirmation (val stringResourceId: Int) {
+}
